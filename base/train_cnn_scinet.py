03be4cc58c91252701517f927f00f4dcac3d86a7
Tiara99 <74646008+Tiara99@users.noreply.github.com>
2024-05-13 18:13:52 +0700

    Update train_cnn_scinet.py
    update function train_cnn_scinet

--- base/train_cnn_scinet.py
+++ base/train_cnn_scinet.py
@@ -21,7 +21,7 @@ def check_Loss_Last(X_test, Y_test, data_type: str):
     print(mae(Y_test, y_hat))
     print('....................')
 
-def train_scinet(   X_train: np.array,
+def train_cnn_scinet( X_train: np.array,
                     y_train: np.array,
                     X_val: np.array,
                     y_val: np.array,
@@ -39,10 +39,13 @@ def train_scinet(   X_train: np.array,
                     dropout = 0.5,
                     loss_weights= [0.2, 0.2, 0.6],
                     learning_rate = 0.01,
-                    probabilistic = False):
+                    probabilistic = False,
+                    cnn_filters = [256, 128, 1],
+                    cnn_kernel_size = [9, 9, 9]):
+                        
+    # def __init__(self, cnn_filters, cnn_kernel_size, output_len, input_len, output_dim, input_dim, selected_columns, loss_weights, hid_size, num_levels, kernel, dropout, learning_rate):
 
-
-    print(f"===========================[SCINET]=====================================")
+    print(f"===========================[CNN-SCINET]=====================================")
     print(f"Initializing training with data:")
     print(f"X_train: {X_train.shape}, y_train: {y_train.shape}")
     print(f"X_val: {X_val.shape}, y_val: {y_val.shape}")
@@ -58,20 +61,34 @@ def train_scinet(   X_train: np.array,
     input_dim = X_train.shape[2]
    # input_len, output_len = X_LEN, Y_LEN
     input_len = X_LEN
-    model = scinet_builder(  output_len= Y_LEN,
-                             output_dim= output_dim,
-                             input_len = input_len,
-                             input_dim= input_dim,
-                             selected_columns= selected_columns,
-                             hid_size= hid_size,
-                             num_levels= num_levels,
-                             kernel = kernel,
-                             dropout = dropout,
-                             loss_weights= loss_weights,
-                             learning_rate = learning_rate, 
-                             probabilistic = probabilistic)
+    # model = scinet_builder(  output_len= Y_LEN,
+                             # output_dim= output_dim,
+                             # input_len = input_len,
+                             # input_dim= input_dim,
+                             # selected_columns= selected_columns,
+                             # hid_size= hid_size,
+                             # num_levels= num_levels,
+                             # kernel = kernel,
+                             # dropout = dropout,
+                             # loss_weights= loss_weights,
+                             # learning_rate = learning_rate, 
+                             # probabilistic = probabilistic)
     
-    
+    model = CNN_SCINet( output_len = Y_LEN,
+                       output_dim = output_dim,
+                       input_len = input_len,
+                       input_dim = input_dim,
+                       selected_columns = selected_columns,
+                       hid_size = hid_size,
+                       num_levels = num_levels,
+                       kernel = kernel,
+                       dropout = dropout,
+                       loss_weights = loss_weights,
+                       learning_rate = learning_rate, 
+                       probabilistic = probabilistic,
+                       cnn_filters = cnn_filters, 
+                       cnn_kernel_size = cnn_kernel_size)
+                                               
     print(model.summary())
 
     # check_Loss_Last(X_train, y_train, 'training')
@@ -108,89 +125,89 @@ def train_scinet(   X_train: np.array,
 
     return model, history, X_train , y_train, X_val, y_val, X_test, y_test
 
-if __name__ == "__main__": 
+# if __name__ == "__main__": 
 
-    #expected dataformat of individual pairs
-    data_format = [
-                                "timestamp",
-                                "open",
-                                "high",
-                                "low",
-                                "close",
-                                "volume",
-                    ]
+#     #expected dataformat of individual pairs
+#     data_format = [
+#                                 "timestamp",
+#                                 "open",
+#                                 "high",
+#                                 "low",
+#                                 "close",
+#                                 "volume",
+#                     ]
 
-    #fraction of dataset used (could be 1, not that the first samples in the dataset are used)
-    fraction_used = 0.01
+#     #fraction of dataset used (could be 1, not that the first samples in the dataset are used)
+#     fraction_used = 0.01
 
-    #train validation test set fractions of used data
-    train_frac = 0.7
-    val_frac = 0.2
-    test_frac = 0.1
+#     #train validation test set fractions of used data
+#     train_frac = 0.7
+#     val_frac = 0.2
+#     test_frac = 0.1
 
-    #predict next Y values based on previous X values
-    X_LEN = 240
-    Y_LEN = 24
+#     #predict next Y values based on previous X values
+#     X_LEN = 240
+#     Y_LEN = 24
 
-    OVERLAPPING = True
-    STANDARDIZE = True
+#     OVERLAPPING = True
+#     STANDARDIZE = True
 
-    RANDOM_SEED = None
+#     RANDOM_SEED = None
 
-    if RANDOM_SEED != None:
-        random.seed(RANDOM_SEED)
+#     if RANDOM_SEED != None:
+#         random.seed(RANDOM_SEED)
 
-    #names of pairs
-    pairs = ["BTCUSD"]
-    data = {}
+#     #names of pairs
+#     pairs = ["BTCUSD"]
+#     data = {}
 
-    for pair in pairs:
-        data[pair] =  pd.read_csv(f"data/{pair}.csv")#.iloc[:10000, :] #debug
-        # print(data[pair].isnull().values.any())
+#     for pair in pairs:
+#         data[pair] =  pd.read_csv(f"data/{pair}.csv")#.iloc[:10000, :] #debug
+#         # print(data[pair].isnull().values.any())
 
-    # Process data:
-    results = preprocess(   data = data, 
-                            symbols = pairs,
-                            data_format = data_format,
-                            fraction = fraction_used,
-                            train_frac = train_frac,
-                            val_frac = val_frac,
-                            test_frac = test_frac,
-                            X_LEN = X_LEN,
-                            Y_LEN = Y_LEN,
-                            OVERLAPPING = OVERLAPPING,
-                            STANDARDIZE = STANDARDIZE
-                            )
+#     # Process data:
+#     results = preprocess(   data = data, 
+#                             symbols = pairs,
+#                             data_format = data_format,
+#                             fraction = fraction_used,
+#                             train_frac = train_frac,
+#                             val_frac = val_frac,
+#                             test_frac = test_frac,
+#                             X_LEN = X_LEN,
+#                             Y_LEN = Y_LEN,
+#                             OVERLAPPING = OVERLAPPING,
+#                             STANDARDIZE = STANDARDIZE
+#                             )
 
-    for result in results:
-        print(f"{result}: {results[result].shape}") 
+#     for result in results:
+#         print(f"{result}: {results[result].shape}") 
 
-    EPOCHS = 1
-    BATCH_SIZE = 350
+#     EPOCHS = 1
+#     BATCH_SIZE = 350
 
-    N_BLOCKS = 2
+#     N_BLOCKS = 2
     
-    training_result  = train_scinet(   
-                    X_train = results["X_train"],
-                    y_train = results["y_train"],
-                    X_val = results["X_val"],
-                    y_val = results["y_val"],
-                    X_test = results["X_test"],
-                    y_test = results["y_test"],
-                    X_LEN = X_LEN,
-                    Y_LEN = [Y_LEN] * N_BLOCKS,
-                    epochs = EPOCHS,
-                    batch_size = BATCH_SIZE,
-                    output_dim = [results["X_train"].shape[2]] * N_BLOCKS,
-                    selected_columns = None,
-                    hid_size= 32,
-                    num_levels= 3,
-                    kernel = 5,
-                    dropout = 0.5,
-                    loss_weights= [0.4, 0.6],
-                    probabilistic = False
-                )
+#     training_result  = train_scinet(   
+#                     X_train = results["X_train"],
+#                     y_train = results["y_train"],
+#                     X_val = results["X_val"],
+#                     y_val = results["y_val"],
+#                     X_test = results["X_test"],
+#                     y_test = results["y_test"],
+#                     X_LEN = X_LEN,
+#                     Y_LEN = [Y_LEN] * N_BLOCKS,
+#                     epochs = EPOCHS,
+#                     batch_size = BATCH_SIZE,
+#                     output_dim = [results["X_train"].shape[2]] * N_BLOCKS,
+#                     selected_columns = None,
+#                     hid_size= 32,
+#                     num_levels= 3,
+#                     kernel = 5,
+#                     dropout = 0.5,
+#                     loss_weights= [0.4, 0.6],
+#                     probabilistic = False
+#                 )
 
-    model = training_result[0]
+#     model = training_result[0]
 
-    model.save_weights(f"model_weights/{'_'.join(pairs)}_{int(time())}")
+#     model.save_weights(f"model_weights/{'_'.join(pairs)}_{int(time())}")
